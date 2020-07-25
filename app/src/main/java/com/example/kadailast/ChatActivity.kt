5544285dc76fc5482221b4f646dcf4e3b7017f82
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-07-26 07:24:30 +0900

    Initial commit

---
+++ app/src/main/java/com/example/kadailast/ChatActivity.kt
@@ -0,0 +1,15 @@
+package com.example.kadailast
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+
+//        TODO: getIntent
+//        TODO: viewMessage
+    }
+}
